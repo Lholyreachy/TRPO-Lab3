c57e07295675c72a6e9c848ba1784f38b24fa75b
Ирина Яценко <lholypeachy@gmail.com>
2021-06-08 01:07:53 +0500

    Создана библиотека

---
+++ TRPO-Lab3.Lib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TRPO_Lab3.Lib
+{
+    public class Class1
+    {
+        public static double Konus(double h, double r1, double r2)
+        {
+            double Konus = (1 / 3) * Math.PI * h * (Math.Pow(r1, 2) + r1 * r2 + Math.Pow(r2, 2));
+
+            return Konus;
+        }
+    }
+}
