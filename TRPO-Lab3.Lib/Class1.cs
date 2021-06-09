a135b7203e429d09ae41047cbc2bb4a583a38f7f
Ирина Яценко <lholypeachy@gmail.com>
2021-06-09 12:59:47 +0500

    Итоговая формула расчета

--- TRPO-Lab3.Lib/Class1.cs
+++ TRPO-Lab3.Lib/Class1.cs
@@ -7,7 +7,7 @@ namespace TRPO_Lab3.Lib
         public static double Konus(double h, double r1, double r2)
           
         {
-            double Konus = (1 / 3) * Math.PI * h * (Math.Pow(r1, 2) + r1 * r2 + Math.Pow(r2, 2));
+            double Konus = Math.PI * h * (Math.Pow(r1, 2) + r1 * r2 + Math.Pow(r2, 2))/3;
             return Konus; 
 
         }
